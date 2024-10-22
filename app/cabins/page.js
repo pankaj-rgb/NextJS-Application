1a5d05eef3c5811f55ae9ab46fed553df9e589fc
Nanami <royalpankaj63@gmail.com>
2024-10-23 00:03:06 +0530

    adding the fetch function to render details in server side

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,18 @@
-import Navigation from "../components/navigation"
+export default async function Pages(){
 
-export default function Pages(){
+    const res=await fetch('https://jsonplaceholder.typicode.com/users');
+    const data=await res.json();
+
+    console.log(data);
+
     return (
         <div>
                {/* <Navigation/>  */}
             <h1>Cabins pages working</h1>
+            <ul>{data.map(user=>
+                <li key={user.id}>{user.name}</li>
+            )}</ul>
         </div>
     );
 }
+
