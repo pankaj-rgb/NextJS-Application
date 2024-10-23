f26aa0b7d40264df2ad8b111d12cfc8dedb7e258
Nanami <royalpankaj63@gmail.com>
2024-10-23 18:56:27 +0530

    adding the google font to download in server and using in the code

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,16 @@ import Logo from "@/app/_components/logo";
 import Navigation from "./_components/navigation";
 
 import '@/app/_styles/globals.css'
+//we could use any google font without downloading best to download on your server
+import {Josefin_Sans} from "next/font/google";
 
+
+const josefin=Josefin_Sans({
+  subsets:['latin'],
+  display:'swap'
+})
+
+console.log(josefin);
 export const metadata={
   // title:"The wild Oasis",
   title:{
@@ -10,14 +19,14 @@ export const metadata={
     default:"Welcome / The Wild Oasis"
   },
   description:"Luxurious Cabin Hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dartk forests",
-  
+
 }
 
 
 export default function RootLayout({children}){
 
   return <html lang='en'>
-    <body className="bg-primary-950 text-primary-100 min-h-screen">
+    <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
       <Logo/>
       <Navigation/>
       <main>
