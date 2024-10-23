cca72bbfc693f5bd0c6ae01b1a222808edab19ce
Nanami <royalpankaj63@gmail.com>
2024-10-23 19:38:37 +0530

    adding the tailwind css component

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Navigation from "./_components/navigation";
 import '@/app/_styles/globals.css'
 //we could use any google font without downloading best to download on your server
 import {Josefin_Sans} from "next/font/google";
+import Header from "./_components/Header";
 
 
 const josefin=Josefin_Sans({
@@ -26,14 +27,16 @@ export const metadata={
 export default function RootLayout({children}){
 
   return <html lang='en'>
-    <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-      <Logo/>
-      <Navigation/>
-      <main>
+    <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+      <Header/>
+      <div className="flex-1 px-8 py-12">
+      <main className="max-w-7xl mx-auto ">
         {children}  
         {/* become layout for all the children component liek the page in which it is present */}
       </main>
-      <footer>Copyright by the Wild Oasis</footer>
+      </div>
+      
+      
     </body>
   </html>
 }
