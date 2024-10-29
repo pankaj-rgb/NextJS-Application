d089f71979c6102b77ee9f73113cf8679a82d40e
Nanami <royalpankaj63@gmail.com>
2024-10-29 18:02:28 +0530

    creting a new guest on first sign up from the session

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google"
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig={
     providers:[
@@ -13,7 +14,24 @@ const authConfig={
         authorized({auth,requests}){
             console.log("auth user ",auth?.user);
             return !!auth?.user; //if user exist true else false
-        }
+        },
+        async signIn({user, account, profile}){
+          try{
+            console.log("sign in user ",user);
+            const existingGuest=await getGuest(user.email);
+            console.log("existing users ",existingGuest);
+            if(!existingGuest) 
+                await createGuest({ email: user.email, fullName: user.name });
+            return true;
+          }catch{ 
+            console.log("error comes here");
+            return false; }  
+        },
+        async session({ session, user }) {
+            const guest = await getGuest(session.user.email);
+            session.user.guestId = guest.id;
+            return session;
+          },
     },
     pages:{
         signIn:"/login",
--- app/account/page.js
+++ app/account/page.js
@@ -7,6 +7,7 @@ export const metadata = {
 export default async function Pages(){
 
   const session=await auth();
+  console.log("account sesion ",session);
     return   <h2 className="font-semibold text-2xl text-accent-400 mb-7">
     Welcome, {session?.user?.name}
   </h2>
