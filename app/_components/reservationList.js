b358631a73946c6e07eb59a1bcaa0a02ee950b7e
Nanami <royalpankaj63@gmail.com>
2024-10-30 18:03:50 +0530

    adding a new loading state and useOptimistic hook for the delete and make user experience better

--- app/_components/reservationList.js
+++ app/_components/reservationList.js
@@ -4,8 +4,9 @@ import { deleteReservation } from '../_lib/action';
 import ReservationCard from './ReservationCard';
 function ReservationList({bookings}){
     //useOptimistic hook  always 2 state , one acutal and one it is function as delete
-    //when no async function performed
+    //when no async function performed , optimisticDelete will trigger the function like dispatch function 
     const [optimisticBookings, optimisticDelete]= 
+    //first argument current state--bookings, secoind argument state update function , current state and next compute of next function  of asyync function and same argument
     useOptimistic(bookings,(curBookings, bookingId)=>{
         return curBookings.filter(booking=>
             booking.id!=bookingId
@@ -16,7 +17,7 @@ function ReservationList({bookings}){
     //optimisticDelete when delete is done
 
     async function handleDelete(bookingId){
-        //immediate delete from the UI 
+        //immediate delete from the UI , optimitically and then actual delete from the function 
         optimisticDelete(bookingId);
         
         await deleteReservation(bookingId);
