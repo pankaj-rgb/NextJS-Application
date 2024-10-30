f4522e687ea15b01e6aae8e193ba580dcc5e02b9
Nanami <royalpankaj63@gmail.com>
2024-10-30 11:17:35 +0530

    delete reservation of the guest booking

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,8 +1,13 @@
+'use client'
 import { TrashIcon } from '@heroicons/react/24/solid';
+import { deleteReservation } from '../_lib/action';
 
 function DeleteReservation({ bookingId }) {
+  //we could have the server action since it is a server component but if it is called by the client component it automatically becomes client component
+  //since this is not aform but a button we couldn't call the server action from form action but instead from button
+
   return (
-    <button className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
+    <button onClick={()=>deleteReservation(bookingId)}  className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
       <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
       <span className='mt-1'>Delete</span>
     </button>
--- app/_lib/action.js
+++ app/_lib/action.js
@@ -21,23 +21,30 @@ export async function updateProfile(formData){
     const {data, error}=await supabase.from('guests')
                         .update(updateData)
                         .eq("id",session.user.guestId);
-    
-    
-    
-                        
-                        
-                        
-                        if(error)
-                            {
-                                throw new Error("guest could not be updated");
-                            }
-//to refresh in real time on the given url path 
+                 
+    if (error) {
+        throw new Error("guest could not be updated");
+    }
+    //to refresh in real time on the given url path 
     revalidatePath('/account/profile');
-
-
 
 }
 
+//it is not a form but a button so called by using bookingId
+export async function deleteReservation(bookingId) {
+    const session = await auth();
+    if (!session) throw new Error('You must be logged in ');
+
+    const { error } = await supabase.from('bookings').delete().eq('id', bookingId);
+
+    if (error) {
+        console.error(error);
+        throw new Error('Booking could not be deleted');
+    }
+
+    revalidatePath('account/reservations');
+
+}
 
 export async function signInAction(){
     //provider of the signIn
